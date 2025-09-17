61b980b06120a28c059c5b76c396d47a5adcd906
Phoug <golheus@gmail.com>
2025-09-17 11:48:15 +0300

    New models

---
+++ Online-Store-ASP.NET/Online-Store-ASP.NET.Shared/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Store_ASP.NET.Shared.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
