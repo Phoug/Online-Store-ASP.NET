0a28ba7cad25cfcc517758b7c996ffb370d82141
Phoug <golheus@gmail.com>
2025-09-17 11:28:17 +0300

    Initial commit with .gitignore and README.md

---
+++ Online-Store-ASP.NET/Online-Store-ASP.NET/Program.cs
@@ -0,0 +1,35 @@
+using Online_Store_ASP.NET.Client.Pages;
+using Online_Store_ASP.NET.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Online_Store_ASP.NET.Client._Imports).Assembly);
+
+app.Run();
